1ac4e09967350bca4b32717da537181f0a319a80
lotteRavn <lotteravn@lottes-mac-mini.local>
2016-05-20 03:00:20 +0200

    Menu

---
+++ BournonvillesAppTest/ContainerViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ContainerViewController.swift
+//  BournonvillesAppTest
+//
+//  Created by Lotte Ravn on 20/05/16.
+//  Copyright © 2016 Lotte Ravn. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+
+    @IBOutlet weak var masterView: UIView!
+    
+    @IBOutlet weak var menuView: UIView!
+    
+    @IBOutlet weak var navView: UIView!
+    
+    @IBOutlet weak var rightConstraintTitleMenu: NSLayoutConstraint!
+    @IBOutlet weak var rightConstraintTop: NSLayoutConstraint!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func menuTapped(sender: AnyObject) {
+    }
+    
+    @IBAction func leftSwipe(sender: UISwipeGestureRecognizer) {
+        rightConstraintTop.constant = 600
+        rightConstraintTitleMenu.constant = 600
+        
+        UIView.animateWithDuration(0.3){
+        self.view.layoutIfNeeded()
+        }
+        
+    }
+
+    @IBAction func rightSwipe(sender: UISwipeGestureRecognizer) {
+        rightConstraintTop.constant = 0
+        rightConstraintTitleMenu.constant = 0
+        
+        UIView.animateWithDuration(0.3){
+            self.view.layoutIfNeeded()
+        }
+
+    }
+}
